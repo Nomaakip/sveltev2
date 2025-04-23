d43ea0c93fb2804362065d2ff1fd868f5e8e42ce
nomaakip! <128547338+Nomaakip@users.noreply.github.com>
2025-04-23 15:28:04 +0200

    a

---
+++ src/lib/warning.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { marked } from "marked";
+  import { fly } from "svelte/transition";
+
+  let { warningText = "undefined warning" } = $props();
+
+  let closed = $state(false);
+
+  const warningIcon = "img/warning.svg";
+  const closeIcon = "img/close.svg";
+</script>
+
+{#if !closed}
+<div class={{ "flex fixed w-[98%] top-2 bg-amber-400 p-5 rounded-md z-1001 gap-3 text-lg" : true, closed: closed}} transition:fly={{ x: -200, duration: 1500, delay: 150}}><img src={warningIcon} alt="warning icon" /><span>{@html marked(warningText)}</span><button class="absolute right-5 hover:cursor-pointer" onclick="{() => (closed = !closed)}"><img src={closeIcon} alt="close icon"/></button></div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,51 @@
 <script>
-	import Kirby from "$lib/kirby.svelte";
-	import ClickButton from "$lib/clickButton.svelte";
-	import Greet from "$lib/greet.svelte";
-	import Head from "$lib/head.svelte";
+  import Kirby from "$lib/kirby.svelte";
+  import ClickButton from "$lib/clickButton.svelte";
+  import Greet from "$lib/greet.svelte";
+  import Head from "$lib/head.svelte";
+  import Warning from "$lib/warning.svelte";
 
-	const version = "1.1.8";
+  const version = "1.2.0-beta_vercel1";
 
-	const backgrounds = [
-		"bg-gray-800",
-		"bg-pink-300",
-		"bg-linear-65 from-red-500 to-pink-500",
-		"bg-linear-65 from-yellow-300 to-emerald-600",
-	];
+  const backgrounds = [
+    "bg-gray-800",
+    "bg-pink-300",
+    "bg-linear-65 from-red-500 to-pink-500",
+    "bg-linear-65 from-yellow-300 to-emerald-600",
+  ];
 
-	let currentBackground = $state(backgrounds[0]);
+  let currentBackground = $state(backgrounds[0]);
 
-	let headConfig = {
-		title: 'welcome',
-		content: 'hell yeah test lol aa beta2'
-	}
+  const headConfig = {
+    title: "welcome",
+    content: "hell yeah test lol aa beta2",
+  };
+
+  const warningText = "check out ***markdown*** on the textbox below!";
 </script>
 
-<Head {...headConfig}/>
+<Head {...headConfig} />
 
 <div
-	id="container"
-	class="{currentBackground} text-white flex flex-col min-h-screen items-center justify-center gap-5 transition delay-200"
+  id="container"
+  class="{currentBackground} text-white flex flex-col min-h-screen items-center justify-center gap-5 transition delay-200"
 >
-	<Greet {version} />
+  <Warning {warningText} />
 
-	<Kirby />
+  <Greet {version} />
 
-	<ClickButton />
+  <Kirby />
 
-	<h1 class="text-2xl">Backgrounds</h1>
-	<div class="flex items-center justify-center flex-wrap gap-5">
-		{#each backgrounds as bg}
-			<button
-				aria-label={bg}
-				class="background h-20 w-40 bg-linear-65 {bg} rounded-md cursor-pointer"
-				onclick={() => (currentBackground = bg)}
-			></button>
-		{/each}
-	</div>
+  <ClickButton />
+
+  <h1 class="text-2xl">Backgrounds</h1>
+  <div class="flex items-center justify-center flex-wrap gap-5">
+    {#each backgrounds as bg}
+      <button
+        aria-label={bg}
+        class="background h-20 w-40 bg-linear-65 {bg} rounded-md cursor-pointer"
+        onclick={() => (currentBackground = bg)}
+      ></button>
+    {/each}
+  </div>
 </div>
